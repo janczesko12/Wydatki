7233e3d2ec90b18b7947a600322c15a5ef8f5446
janczesko12 <janczesko12@gmail.com>
2026-08-28 21:46:09 +0200

    Naprawa braku google-services.json na GitHub Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.wydatki"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
