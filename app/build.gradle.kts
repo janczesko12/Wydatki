4b373159d05a91eacefc7f38b344fbe572625510
janczesko12 <janczesko12@gmail.com>
2026-08-28 21:49:27 +0200

    Naprawa dekodowania google-services.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.wydatki"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
