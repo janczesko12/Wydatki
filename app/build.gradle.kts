16e7d69fdcbf1e45db186dd5f95fa34f950aa5a9
janczesko12 <janczesko12@gmail.com>
2026-08-28 22:08:10 +0200

    Poprawa haseł podpisywaniav2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.wydatki"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
