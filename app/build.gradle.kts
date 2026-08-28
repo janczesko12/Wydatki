5a7b8325d4e63aa92e369fb0d00db418f8c5ebb1
janczesko12 <janczesko12@gmail.com>
2026-08-28 21:38:34 +0200

    Włączenie podpisywania APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.wydatki"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0."
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
